7552386db13a00eccfbe0f3eabf6bba158cf3e1e
Latesun <latesun.lee@gmail.com>
2025-04-20 23:46:57 +0800

    feat: add dashboard

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -5,6 +5,7 @@ map("i", "jk", "<ESC>")
 
 map({ "n", "i", "v" }, "<leader>w", "<cmd> w <CR><ESC>")
 map({ "n", "i", "v" }, "<leader>wa", "<cmd> wa <CR><ESC>")
+map("n", "<leader>a", "<cmd> Dashboard <CR>")
 map("n", "<leader>l", "<cmd> Lazy <CR>")
 map("n", "<leader>m", "<cmd> Mason <CR>")
 map("n", "<leader>k", "<cmd> Interestingwords --toggle <CR>")
