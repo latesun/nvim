af65f82dfa9e7eaf0ad027e6682364732a375f60
Latesun <latesun.lee@gmail.com>
2025-05-04 11:49:53 +0800

    feat: add gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	lazy = false,
+	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "A" },
+				change = { text = "M" },
+				delete = { text = "D" },
+				topdelete = { text = "D" },
+				changedelete = { text = "D" },
+				untracked = { text = "U" },
+			},
+
+			current_line_blame = true,
+		})
+	end,
+}
