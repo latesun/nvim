bb2b157a8fe10450cef647d70084c460b0726062
Latesun <latesun.lee@gmail.com>
2025-05-05 12:06:21 +0800

    feat: add more cfg for gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -11,7 +11,14 @@ return {
 				changedelete = { text = "D" },
 				untracked = { text = "U" },
 			},
-
+			signs_staged = {
+				add = { text = "A" },
+				change = { text = "M" },
+				delete = { text = "D" },
+				topdelete = { text = "D" },
+				changedelete = { text = "D" },
+				untracked = { text = "U" },
+			},
 			current_line_blame = true,
 		})
 	end,
