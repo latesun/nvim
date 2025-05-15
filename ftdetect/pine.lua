b7d6fe8c12f8bd17caf2168d4c3dcf2cc727a2a8
Latesun <latesun.lee@gmail.com>
2025-05-16 01:53:32 +0800

    feat(pine): add more rules

--- ftdetect/pine.lua
+++ ftdetect/pine.lua
@@ -9,11 +9,12 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 		vim.cmd([[
 			syntax match pineComment "//.*$"
 			syntax keyword pineBoolean true false
-			syntax keyword pineKeyword if else and or not na
-			syntax match pineVariable '\([a-zA-Z0-9]\+\)\ze\s*='
+			syntax keyword pineKeyword if else and or not na var float bool array color string type for
+			syntax match pineVariable '\([a-zA-Z0-9_]\+\)\ze\s*:\?=\@='
 			syntax match pineFunction '\([a-zA-Z0-9]\+\)\ze\s*('
 			syntax match pineString '"[^"]*"'
 			syntax match pinePackage '\([a-zA-Z]\+\)\.'
+			syntax match pineConstant '\<[a-zA-Z0-9_]\+_\w*\>'
 
 			highlight link pineComment Comment
 			highlight link pineBoolean Boolean
@@ -22,6 +23,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 			highlight link pineFunction Function
 			highlight link pineString String
 			highlight link pinePackage Identifier
+			highlight link pineConstant Constant
 		]])
 	end,
 })
