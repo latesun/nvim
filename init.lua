84c40971a39fa5b5124effc74e3b413763f533d1
Latesun <latesun.lee@gmail.com>
2025-05-16 01:53:50 +0800

    feat: reopen last edit

--- init.lua
+++ init.lua
@@ -4,3 +4,14 @@ require("config.options")
 
 vim.diagnostic.config({ virtual_text = true })
 vim.lsp.enable({ "lua_ls", "pylsp" })
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local line = mark[1]
+		local col = mark[2]
+		local buf_name = vim.api.nvim_buf_get_name(0)
+		if line > 0 and line <= vim.api.nvim_buf_line_count(0) and buf_name ~= "" then
+			pcall(vim.api.nvim_win_set_cursor, 0, { line, col })
+		end
+	end,
+})
