57f6089c4c323a5360fdf2dd2d40f4cf6ec69c4e
Latesun <latesun.lee@gmail.com>
2026-09-06 11:04:50 +0800

    feat: add lint checker

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,16 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPost", "BufWritePost" },
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			go = { "golangcilint" },
+		}
+
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+	end,
+}
